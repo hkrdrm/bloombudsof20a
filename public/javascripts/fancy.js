2e8d6ad41be4825f49ac99fa4c4af2c13728c2fd
Matt <hkr.drm@gmx.com>
2015-01-19 18:25:46 -0600

    figured out fancybox control images need to go in same directory as the stylesheets

--- public/javascripts/fancy.js
+++ public/javascripts/fancy.js
@@ -35,4 +35,21 @@ $(document).ready(function() {
       }
     }
   });
+
+
+  $(".fancybox-button").fancybox({
+    prevEffect		: 'none',
+    nextEffect		: 'none',
+    closeBtn		: false,
+    helpers		: {
+      title	: { type : 'inside' },
+      buttons	: {}
+    }
+  });
+
+  $(".fancybox").fancybox({
+    openEffect	: 'none',
+    closeEffect	: 'none'
+  });
+
 });

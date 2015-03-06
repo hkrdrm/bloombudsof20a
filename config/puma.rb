71f7cd2b1de023c5b23f3490116f6110e6fcbfb0
Matt <hkr.drm@gmx.com>
2015-03-05 19:40:38 -0600

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,10 @@
 environment ENV['RAILS_ENV'] || 'production'
-bind 'unix:///tmp/my_app.sock'
+bind 'unix:///tmp/bloomingbuds.sock'
 daemonize false
 
 workers    1    # should be same number of your CPU core
 threads    1, 3 # min, max
 
-pidfile    "/home/hkrdrm/tmp/puma/puma_app1.pid" # puma folder must exist
+pidfile    "/home/hkrdrm/tmp/puma/puma_bloomingBuds.pid" # puma folder must exist
 state_path "/home/hkrdrm/tmp/puma/state"
 activate_control_app
